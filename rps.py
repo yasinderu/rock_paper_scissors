e4d95dcb7ae9fd3f1306fb30cd5ff74b590c84e1
yasinderu <m.yasin449@gmail.com>
2024-05-04 17:26:46 +0700

    first commit

---
+++ rps.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+class RPS:
+    def __init__(self):
+        print('Welcome to Rock Paper Scissors game!')
+
+        self.move: dict = {'rock': '🪨', 'scissors': '✂️', 'paper': '📜'}
+        self.valid_moves: list[str] = list(self.move.keys())
+
+        def play_game(self):
+            pass
+
+        def display_move(self):
+            pass
+
+        def check_move(self):
+            pass
